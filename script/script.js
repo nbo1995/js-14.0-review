a9cb9838635cbb02b0c74b9b53f33cffff69eb04
Sergey Stepanenko <nbo1995@gmail.com>
2020-08-29 10:52:19 +0400

    first commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let message = function () {
+  alert('Изучил git и github');
+};
+
+let money = "100000000 руб"
+let income = "100000 руб"
+let addExpenses = "30000 руб"
+let deposit = "0 руб"
+let mission = "Научиться верстать полноценные сайты"
+let period = "4 месяца"
+
+console.log(mission);
+
+
+document.getElementById('github').addEventListener('click', message);
